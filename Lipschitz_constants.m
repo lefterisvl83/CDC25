9f7a708d03c47b75a9e12dc1f6a60a90879ef8c6
Lefteris Vlahakis <34964419+lefterisvl83@users.noreply.github.com>
2025-04-14 14:41:43 +0200

    Update Lipschitz_constants.m

--- Lipschitz_constants.m
+++ Lipschitz_constants.m
@@ -19,13 +19,5 @@ function L = calculateLipschitzConstant(P)
     L = norm(A, 'inf');
 end
 
-function gamma=min_max(a,E,neg)
-%
-er=sdpvar(2,1);
-F = [E(end).A*er<=E(end).b];
-ops = sdpsettings;
-obj = -neg*a*er;
-optimize(F, obj, ops);
-gamma=a*value(er);
 %
 end
